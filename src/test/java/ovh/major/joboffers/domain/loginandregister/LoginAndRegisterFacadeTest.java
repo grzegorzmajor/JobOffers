f642d19b6a11400bade55adb6175cf847c56bd71
grzegorzmajor <major.grzegorz@gmail.com>
2023-03-15 09:34:26 +0100

    added empty tested methods, implemented first test method, back to hamcrest and changed that in pom.xml

--- src/test/java/ovh/major/joboffers/domain/loginandregister/LoginAndRegisterFacadeTest.java
+++ src/test/java/ovh/major/joboffers/domain/loginandregister/LoginAndRegisterFacadeTest.java
@@ -1,11 +1,47 @@
 package ovh.major.joboffers.domain.loginandregister;
 
 import org.junit.jupiter.api.Test;
+import ovh.major.joboffers.domain.loginandregister.dto.RegisterUserDto;
+import ovh.major.joboffers.domain.loginandregister.dto.RegistrationResultDto;
+import ovh.major.joboffers.domain.loginandregister.dto.UserDto;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
 
 public class LoginAndRegisterFacadeTest {
 
+    LoginAndRegisterFacade loginAndRegisterFacade;
+
     @Test
-    public void test(){
+    public void shouldThrowExceptionWhenUserNotFound() {
+        //given
+        String username = "user";
+
+        //when
+       // Throwable thrown = catchTrowable()
+
+        //then
+
+    }
+
+    @Test
+    public void shouldFindUserByUserName() {
+        //given
+        RegisterUserDto registerUserDto = new RegisterUserDto("name", "pass");
+        RegistrationResultDto register = loginAndRegisterFacade.register(registerUserDto);
+
+        //when
+        UserDto userResult = loginAndRegisterFacade.findByUsername(register.name());
+
+        //then
+        assertThat(userResult,is(equalTo(new UserDto(register.id(), "name", "pass"))));
+
+    }
+
+    @Test
+    public void shouldRegister_user() {
 
     }
 }
