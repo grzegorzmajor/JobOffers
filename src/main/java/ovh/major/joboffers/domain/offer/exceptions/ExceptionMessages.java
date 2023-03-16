646ac082f546a69b8541d31d97f590002a5977ca
grzegorzmajor <major.grzegorz@gmail.com>
2023-03-16 19:06:04 +0100

    implemented OfferService

--- src/main/java/ovh/major/joboffers/domain/offer/exceptions/ExceptionMessages.java
+++ src/main/java/ovh/major/joboffers/domain/offer/exceptions/ExceptionMessages.java
@@ -2,7 +2,8 @@ package ovh.major.joboffers.domain.offer.exceptions;
 
 public enum ExceptionMessages {
     OFFER_NOT_FOUND("Offer not found!"),
-    DUPLICATE_KEY("Offer key duplicated!");
+    DUPLICATE_KEY("Offer key duplicated!"),
+    DUPLICATED_OFFER("Offer is exist in repository!");
 
     ExceptionMessages(String s) {
     }
