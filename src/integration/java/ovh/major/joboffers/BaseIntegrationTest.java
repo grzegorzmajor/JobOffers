fa5d968c3c3869ab6373a9bf54379b860d6ed10b
grzegorzmajor <major.grzegorz@gmail.com>
2023-03-23 12:53:54 +0100

    added testcontainers dependency, added empty integration tests configuration class

---
+++ src/integration/java/ovh/major/joboffers/BaseIntegrationTest.java
@@ -0,0 +1,8 @@
+package ovh.major.joboffers;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = JobOffersSpringBootApplication.class)
+
+public class BaseIntegrationTest {
+}
