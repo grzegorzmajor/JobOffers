1b9c78c446d0f808704dd47508b8cdc28d69f119
major.grzegorz <major.grzegorz@gmail.com>
2023-05-01 18:32:25 +0200

    added $Schema annotation with description and example.

--- src/main/java/ovh/major/joboffers/domain/loginandregister/dto/RegisterUserDto.java
+++ src/main/java/ovh/major/joboffers/domain/loginandregister/dto/RegisterUserDto.java
@@ -1,7 +1,19 @@
 package ovh.major.joboffers.domain.loginandregister.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record RegisterUserDto(
+
+        @Schema(
+                description = "User name",
+                example = "userName"
+        )
         String name,
+
+        @Schema(
+                description = "Password",
+                example = "pass"
+        )
         String password
 ) {
 }
